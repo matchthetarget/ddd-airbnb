4074bb42c5e27d8169e1ddc43f79f739e7764b73
anonymous anonymous <jelani@firstdraft.com>
2022-02-19 21:36:22 +0000

    Whitelist columns in Stay for ActiveAdmin

--- app/admin/stays.rb
+++ app/admin/stays.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Stay do
 
+ permit_params :guest_id, :room_id, :check_in, :check_out, :approved
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
