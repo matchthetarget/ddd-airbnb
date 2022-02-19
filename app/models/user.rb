e8475652777f1905c99776d1076e29f27011fe24
anonymous anonymous <jelani@firstdraft.com>
2022-02-19 21:36:01 +0000

    Direct association between users and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :dependent => :destroy
+
   has_many   :recieved_messages,
              :class_name => "Message",
              :foreign_key => "recipient_id",
